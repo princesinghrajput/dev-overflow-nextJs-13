f84a2e8a29dd701d20fb1e9908540218a3f06905
princesinghrajput <psr8084@gmail.com>
2024-03-08 00:18:31 +0530

    Configured ESLint, Prettier, Tailwind, and CSS styles

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description:
+    "A community-driven platform for asking and answering questions about software development. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, game development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
