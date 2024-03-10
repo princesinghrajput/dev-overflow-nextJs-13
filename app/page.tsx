f3648f6b52402b1121acedc2cf042d6116f432b9
princesinghrajput <psr8084@gmail.com>
2024-03-10 17:53:17 +0530

    Added auth using Clerk

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Prince Singh Rajput kdbkjbksdb</h1>
-      <h2 className="h2-bold">Prince Singh Rajput kdbkjbksdb</h2>
-    </div>
-  );
-}
